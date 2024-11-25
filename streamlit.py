eebcd7500230cdcdc66fcf538b8ed148884f8a66
clarachalayer <117898057+clarachalayer@users.noreply.github.com>
2024-11-25 18:09:02 +0100

    Update streamlit.py

--- streamlit.py
+++ streamlit.py
@@ -1,7 +1,7 @@
 
 import streamlit as st
 import pandas as pd
-import plotly.express as px
+import plotly
 
 # Configurer la page de l'application
 st.set_page_config(page_title="A Random App", layout="wide")
